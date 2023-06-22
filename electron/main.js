34df8cb2b1605439f4d6d5fbf8fe2188b99d371e
Ihar Krauchuk <ihar.krauchuk@gmail.com>
2023-06-22 14:36:17 +0200

    used create-react-app

--- electron/main.js
+++ electron/main.js
@@ -1,16 +1,19 @@
 const path = require('path')
 const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
     },
   })
 
-  win.loadFile(path.join(__dirname, 'index.html'))
+  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
+
+  win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
